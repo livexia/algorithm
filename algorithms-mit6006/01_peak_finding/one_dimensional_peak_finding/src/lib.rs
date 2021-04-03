a88f85aa09105cb76f93ab319c9dd7dd77f1e263
livexia <istillalive@live.cn>
2021-04-03 22:04:02 +0800

    add few more test

--- algorithms-mit6006/01_peak_finding/one_dimensional_peak_finding/src/lib.rs
+++ algorithms-mit6006/01_peak_finding/one_dimensional_peak_finding/src/lib.rs
@@ -53,7 +53,125 @@ mod tests {
     use rand::{thread_rng, Rng};
 
     #[test]
-    fn it_works() {
+    fn empty() {
+        let nums = [0; 0];
+        if let Some((_, index)) = one_dimensional_peak_finding(&nums, 0) {
+            assert!(is_peak(&nums, index));
+            assert!(false)
+        } else {
+            assert!(true);
+        }
+    }
+
+    #[test]
+    fn one() {
+        let nums = [100];
+        if let Some((_, index)) = one_dimensional_peak_finding(&nums, 0) {
+            assert!(is_peak(&nums, index));
+            assert_eq!(index, 0);
+        } else {
+            println!("{:?}", nums);
+            assert!(false);
+        }
+    }
+
+    #[test]
+    fn two() {
+        let nums = [100, 999];
+        if let Some((_, index)) = one_dimensional_peak_finding(&nums, 0) {
+            assert!(is_peak(&nums, index));
+            assert_eq!(index, 1);
+        } else {
+            println!("{:?}", nums);
+            assert!(false);
+        }
+    }
+
+    #[test]
+    fn two2() {
+        let nums = [10000, 999];
+        if let Some((_, index)) = one_dimensional_peak_finding(&nums, 0) {
+            assert!(is_peak(&nums, index));
+            assert_eq!(index, 0);
+        } else {
+            println!("{:?}", nums);
+            assert!(false);
+        }
+    }
+
+    #[test]
+    fn three1() {
+        let nums = [10000, 999, 0];
+        if let Some((_, index)) = one_dimensional_peak_finding(&nums, 0) {
+            assert!(is_peak(&nums, index));
+            assert_eq!(index, 0);
+        } else {
+            println!("{:?}", nums);
+            assert!(false);
+        }
+    }
+
+    #[test]
+    fn three2() {
+        let nums = [0, 1, 999];
+        if let Some((_, index)) = one_dimensional_peak_finding(&nums, 0) {
+            assert!(is_peak(&nums, index));
+            assert_eq!(index, 2);
+        } else {
+            println!("{:?}", nums);
+            assert!(false);
+        }
+    }
+
+    #[test]
+    fn flat() {
+        let nums = [100; 20];
+        if let Some((_, index)) = one_dimensional_peak_finding(&nums, 0) {
+            assert!(is_peak(&nums, index));
+            assert_eq!(index, 10);
+        } else {
+            println!("{:?}", nums);
+            assert!(false);
+        }
+    }
+
+    #[test]
+    fn descending() {
+        let nums = [100, 99, 80, 70, 20, 10, 9, 8, 0, -1, -10, -100, -200];
+        if let Some((_, index)) = one_dimensional_peak_finding(&nums, 0) {
+            assert!(is_peak(&nums, index));
+            assert_eq!(index, 0);
+        } else {
+            println!("{:?}", nums);
+            assert!(false);
+        }
+    }
+
+    #[test]
+    fn asscending() {
+        let nums = [-100, -99, -80, -70, -20, -10, -9, -8, 0, 1, 10, 100, 200, 999];
+        if let Some((result, index)) = one_dimensional_peak_finding(&nums, 0) {
+            assert!(is_peak(&nums, index));
+            assert_eq!(result, 999);
+        } else {
+            println!("{:?}", nums);
+            assert!(false);
+        }
+    }
+
+    #[test]
+    fn mid() {
+        let nums = [-100, -99, -80, -70, -20, -10, -9, 222, 0, 1, 10, 100, 200, 999];
+        if let Some((result, index)) = one_dimensional_peak_finding(&nums, 0) {
+            assert!(is_peak(&nums, index));
+            assert_eq!(result, 222);
+        } else {
+            println!("{:?}", nums);
+            assert!(false);
+        }
+    }
+    #[test]
+    fn random_test() {
         for _ in 0..10_000 {
             let mut nums = [0i8; 256];
             thread_rng().fill(&mut nums);
