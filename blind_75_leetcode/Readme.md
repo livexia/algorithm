2427d3274408143bec4908ec4f719bdd7a471c7f
livexia <istillalive@live.cn>
2022-08-29 11:48:10 +0800

    upload blind-75 template

---
+++ blind_75_leetcode/Readme.md
@@ -0,0 +1,7 @@
+# Try to finish Blind 75 LeetCode Questions
+
+Run `cargo test test_{id}` to run the solution for "question #id" 
+
+Question list: https://leetcode.com/discuss/general-discussion/460599/blind-75-leetcode-questions
+
+Leetcode Template: https://github.com/aylei/leetcode-rust
---
+++ blind_75_leetcode/src/solution/s0001_two_sum.rs
@@ -0,0 +1,30 @@
+pub struct Solution {}
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map: HashMap<i32, usize> = HashMap::new();
+        let mut result = vec![];
+        for (index, &num) in nums.iter().enumerate() {
+            if let Some(&other) = map.get(&(target - num)) {
+                result = vec![other as i32, index as i32]
+            } else {
+                map.insert(num, index);
+            }
+        }
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        assert_eq!(vec![0, 1], Solution::two_sum(vec![2, 7, 11, 15], 9));
+        assert_eq!(vec![1, 2], Solution::two_sum(vec![3, 2, 4], 6));
+        assert_eq!(vec![0, 1], Solution::two_sum(vec![3, 3], 6));
+    }
+}
