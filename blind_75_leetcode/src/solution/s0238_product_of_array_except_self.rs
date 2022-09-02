615e060ddb75401dd86ef35e271da536e8b3d57b
livexia <istillalive@live.cn>
2022-09-02 19:42:37 +0800

    finish solution with O(N) space

--- blind_75_leetcode/src/solution/s0238_product_of_array_except_self.rs
+++ blind_75_leetcode/src/solution/s0238_product_of_array_except_self.rs
@@ -3,28 +3,19 @@ pub struct Solution {}
 
 impl Solution {
     pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-        let mut zero_count = 0;
-        let mut zero_index = 0;
-        let total_product = nums.iter().enumerate().fold(1, |s, (index, num)| {
-            if num == &0 {
-                zero_count += 1;
-                zero_index = index;
-                s
-            } else {
-                s * num
-            }
-        });
-        let mut answer = vec![];
-        if zero_count > 1 {
-            answer = vec![0; nums.len()];
-        } else if zero_count == 1 {
-            answer = vec![0; nums.len()];
-            answer[zero_index] = total_product;
-        } else {
-            for num in nums {
-                answer.push(total_product / num)
-            }
+        let l = nums.len();
+        let mut answer = vec![1; l];
+        for i in 1..l {
+            answer[i] = nums[i - 1] * answer[i - 1]; // calculate prefix product
         }
+        let mut suffix_product = vec![1; l];
+        for i in (0..l - 1).rev() {
+            suffix_product[i] = nums[i + 1] * suffix_product[i + 1]; // calculate suffix product
+        }
+        for i in 0..l {
+            answer[i] *= suffix_product[i];
+        }
+
         answer
     }
 }
