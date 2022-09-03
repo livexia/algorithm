6c4873bafb83161ebb44d8a597de987930cbd759
livexia <istillalive@live.cn>
2022-09-03 15:01:15 +0800

    53 with timeout

---
+++ blind_75_leetcode/src/solution/s0053_maximum_subarray.rs
@@ -0,0 +1,36 @@
+#![allow(dead_code)]
+pub struct Solution {}
+
+impl Solution {
+    pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+        let l = nums.len();
+        let mut prefix_sum = vec![0; l];
+        prefix_sum[0] = nums[0];
+        for i in 1..l {
+            prefix_sum[i] = prefix_sum[i - 1] + nums[i];
+        }
+        let mut ans = nums[0];
+        for i in 0..l {
+            for j in 0..=i {
+                ans = ans.max(prefix_sum[i] - prefix_sum[j] + nums[j]);
+            }
+        }
+
+        ans
+    }
+}
+
+#[cfg(test)]
+mod tests_53 {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(
+            Solution::max_sub_array(vec![-2, 1, -3, 4, -1, 2, 1, -5, 4]),
+            6
+        );
+        assert_eq!(Solution::max_sub_array(vec![1]), 1);
+        assert_eq!(Solution::max_sub_array(vec![5, 4, -1, 7, 8]), 23);
+    }
+}
