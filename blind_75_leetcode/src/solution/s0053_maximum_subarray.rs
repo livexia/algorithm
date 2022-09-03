7736c4e0dec2d74d8571bdb21fa881d62ba863c1
livexia <istillalive@live.cn>
2022-09-03 17:05:34 +0800

    remove unnecessary use of intermediate vector and loop

--- blind_75_leetcode/src/solution/s0053_maximum_subarray.rs
+++ blind_75_leetcode/src/solution/s0053_maximum_subarray.rs
@@ -3,19 +3,14 @@ pub struct Solution {}
 
 impl Solution {
     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
-        let l = nums.len();
-        let mut prefix_sum = vec![0; l];
-        prefix_sum[0] = nums[0];
-        for i in 1..l {
-            prefix_sum[i] = prefix_sum[i - 1] + nums[i];
-        }
+        let mut prefix = 0;
         let mut ans = nums[0];
-        let mut left = 0;
-        for p in prefix_sum {
-            ans = ans.max(p - left);
-            left = left.min(p);
+        let mut least_prefix = 0;
+        for num in nums {
+            prefix = prefix + num;
+            ans = ans.max(prefix - least_prefix);
+            least_prefix = least_prefix.min(prefix);
         }
-
         ans
     }
 }
