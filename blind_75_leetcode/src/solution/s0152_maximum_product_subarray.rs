3601c8830098f7163bd09e8eb8ae82f1354ac387
livexia <istillalive@live.cn>
2022-09-20 21:37:32 +0800

    add prefix product solution

--- blind_75_leetcode/src/solution/s0152_maximum_product_subarray.rs
+++ blind_75_leetcode/src/solution/s0152_maximum_product_subarray.rs
@@ -13,6 +13,33 @@ impl Solution {
             ans = ans.max(dp.1);
         }
         ans
+        // with prefix product
+        // let mut prefix_product = vec![1; nums.len()];
+        // prefix_product[0] = nums[0];
+        // for i in 1..nums.len() {
+        //     if prefix_product[i - 1] == 0 {
+        //         prefix_product[i] = nums[i];
+        //         continue;
+        //     }
+        //     prefix_product[i] = prefix_product[i - 1] * nums[i];
+        // }
+        // let mut min_minus_prefix_product = 1;
+        // let mut ans = nums[0];
+        // for p in prefix_product {
+        //     if p < 0 {
+        //         ans = ans.max(p / min_minus_prefix_product);
+        //         if min_minus_prefix_product == 1 {
+        //             min_minus_prefix_product = p;
+        //         }
+        //         min_minus_prefix_product = min_minus_prefix_product.max(p);
+        //     } else {
+        //         if p == 0 {
+        //             min_minus_prefix_product = 1
+        //         }
+        //         ans = ans.max(p);
+        //     }
+        // }
+        // ans
     }
 }
 
@@ -29,5 +56,6 @@ mod tests_153 {
         assert_eq!(Solution::max_product(vec![0, 2]), 2);
         assert_eq!(Solution::max_product(vec![-3, 0, 1, -2]), 1);
         assert_eq!(Solution::max_product(vec![0, 1, 0]), 1);
+        assert_eq!(Solution::max_product(vec![2, 3, -1, -2, 4, -5]), 40);
     }
 }
