9dabedde33b03cb9e4d088137aed77c436ecf8f7
livexia <istillalive@live.cn>
2022-09-23 16:42:11 +0800

    upload solution

--- blind_75_leetcode/src/solution/s0011_container_with_most_water.rs
+++ blind_75_leetcode/src/solution/s0011_container_with_most_water.rs
@@ -4,20 +4,19 @@ pub struct Solution {}
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
         let l = height.len();
+        let mut left = 0;
+        let mut right = l - 1;
         let mut ans = 0;
-        for left in 0..l {
-            for right in left + 1..l {
-                ans = ans.max(volume(&height, left, right))
+        while left < right {
+            ans = ans.max((height[right].min(height[left])) * (right - left) as i32);
+            if height[left] < height[right] {
+                left += 1;
+            } else {
+                right -= 1;
             }
         }
         ans
     }
-}
-
-fn volume(height: &[i32], left: usize, right: usize) -> i32 {
-    let width = (right - left) as i32;
-    let h = height[left].min(height[right]);
-    width * h
 }
 
 #[cfg(test)]
@@ -27,6 +26,8 @@ mod tests_11 {
     #[test]
     fn it_works() {
         assert_eq!(Solution::max_area(vec![1, 8, 6, 2, 5, 4, 8, 3, 7]), 49);
+        assert_eq!(Solution::max_area(vec![1, 10, 6, 2, 5, 4, 10, 3, 7]), 50);
         assert_eq!(Solution::max_area(vec![1, 1]), 1);
+        assert_eq!(Solution::max_area(vec![1, 3, 2, 5, 25, 24, 5]), 24);
     }
 }
