1b3ec68e1f23b9c7411d0d9403b5cab1329567f7
livexia <istillalive@live.cn>
2022-09-23 10:21:42 +0800

    with passable solution

--- blind_75_leetcode/src/solution/s0015_3sum.rs
+++ blind_75_leetcode/src/solution/s0015_3sum.rs
@@ -4,10 +4,13 @@ pub struct Solution {}
 impl Solution {
     pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
         use std::collections::HashMap;
+        use std::collections::HashSet;
         let mut nums = nums;
         nums.sort();
         let l = nums.len();
         let mut ans = vec![];
+
+        let mut shown = HashSet::new();
 
         for i in 0..l {
             let mut map = HashMap::new();
@@ -17,11 +20,17 @@ impl Solution {
                     continue;
                 }
                 match map.get(&(-target - nums[j])) {
-                    Some(_) => ans.push(vec![target, -target - nums[j], nums[j]]),
+                    Some(_) => {
+                        let mut res = vec![target, -target - nums[j], nums[j]];
+                        res.sort();
+                        if shown.insert(res.clone()) {
+                            ans.push(res);
+                        }
+                    }
                     None => {
                         map.insert(nums[j], j);
                     }
-                };
+                }
             }
         }
         ans
@@ -31,14 +40,23 @@ impl Solution {
 #[cfg(test)]
 mod tests_15 {
     use super::*;
+    fn is_equal(left: &[Vec<i32>], right: &[Vec<i32>]) -> bool {
+        use std::collections::HashSet;
+        let left: HashSet<&Vec<i32>> = left.iter().collect();
+        let right: HashSet<&Vec<i32>> = right.iter().collect();
+        left == right
+    }
 
     #[test]
     fn it_works() {
-        assert_eq!(
-            Solution::three_sum(vec![-1, 0, 1, 2, -1, -4]),
-            vec![vec![-1, -1, 2], vec![-1, 0, 1]]
-        );
+        assert!(is_equal(
+            &Solution::three_sum(vec![-1, 0, 1, 2, -1, -4]),
+            &vec![vec![-1, -1, 2], vec![-1, 0, 1]]
+        ));
         assert!(Solution::three_sum(vec![0, 1, 1]).is_empty());
-        assert_eq!(Solution::three_sum(vec![0, 0, 0]), vec![vec![0, 0, 0]]);
+        assert!(is_equal(
+            &Solution::three_sum(vec![0, 0, 0]),
+            &vec![vec![0, 0, 0]]
+        ));
     }
 }
