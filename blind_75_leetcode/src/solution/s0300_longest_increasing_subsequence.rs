9cac03725fd801ec9ad88d98ef3cc07f5ea98975
livexia <istillalive@live.cn>
2022-09-26 20:26:07 +0800

    with min-heap

--- blind_75_leetcode/src/solution/s0300_longest_increasing_subsequence.rs
+++ blind_75_leetcode/src/solution/s0300_longest_increasing_subsequence.rs
@@ -3,16 +3,39 @@ pub struct Solution {}
 
 impl Solution {
     pub fn length_of_lis(nums: Vec<i32>) -> i32 {
-        let mut dp = vec![1; nums.len()];
+        use std::cmp::Reverse;
+        use std::collections::BinaryHeap;
+
+        if nums.is_empty() {
+            return 0;
+        }
+        let mut init_heap = BinaryHeap::new();
+        init_heap.push(Reverse(nums[0]));
+
+        let mut dp = vec![];
+        dp.push(init_heap);
 
         for i in 1..nums.len() {
-            for j in 0..i {
-                if nums[i] > nums[j] {
-                    dp[i] = dp[i].max(dp[j] + 1);
+            for count in (0..dp.len()).rev() {
+                if let Some(min) = dp[count].peek() {
+                    if min <= &Reverse(nums[i]) {
+                        if count == 0 {
+                            dp[count].push(Reverse(nums[i]))
+                        }
+                    } else {
+                        if count == dp.len() - 1 {
+                            let mut new_heap = BinaryHeap::new();
+                            new_heap.push(Reverse(nums[i]));
+                            dp.push(new_heap);
+                        } else {
+                            dp[count + 1].push(Reverse(nums[i]))
+                        }
+                        break;
+                    }
                 }
             }
         }
-        dp.into_iter().max().unwrap_or(0)
+        dp.len() as i32
     }
 }
 
