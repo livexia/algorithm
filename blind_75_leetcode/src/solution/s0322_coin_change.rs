d8c0616d6f3e9613da2a66b449a0f36eff2a95e3
livexia <istillalive@live.cn>
2022-09-26 14:27:04 +0800

    trim more path

--- blind_75_leetcode/src/solution/s0322_coin_change.rs
+++ blind_75_leetcode/src/solution/s0322_coin_change.rs
@@ -7,19 +7,26 @@ impl Solution {
             return 0;
         }
         let mut coins = coins;
-        let mut amount = amount;
         coins.sort();
-        let mut ans = 0;
-        while let Some(coin) = coins.pop() {
-            if amount >= coin {
-                ans += amount / coin;
-                amount %= coin;
-            }
-            if amount == 0 {
+        let mut path = vec![(amount, 0)];
+        let mut ans = amount / coins[0] + 1;
+
+        while let Some((remain, count)) = path.pop() {
+            if remain == 0 {
+                ans = ans.min(count);
                 break;
             }
+            if ans <= count + 1 || remain < coins[0] {
+                continue;
+            }
+            for c in &coins {
+                if c > &remain {
+                    break;
+                }
+                path.push((remain - c, count + 1));
+            }
         }
-        if ans == 0 || amount != 0 {
+        if ans == amount / coins[0] + 1 {
             -1
         } else {
             ans
@@ -36,6 +43,9 @@ mod tests_322 {
         assert_eq!(Solution::coin_change(vec![1, 2, 5], 11), 3);
         assert_eq!(Solution::coin_change(vec![2], 3), -1);
         assert_eq!(Solution::coin_change(vec![1], 0), 0);
+        assert_eq!(Solution::coin_change(vec![4, 3, 2], 9), 3);
+        assert_eq!(Solution::coin_change(vec![4, 3, 2, 8], 9), 3);
+        assert_eq!(Solution::coin_change(vec![4, 3, 2, 8], 10), 2);
         assert_eq!(Solution::coin_change(vec![186, 419, 83, 408], 6249), 20);
     }
 }
