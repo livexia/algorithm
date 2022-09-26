7c95dd91627dff79859de6d92eb1e46ac3a0ac03
livexia <istillalive@live.cn>
2022-09-26 20:31:57 +0800

    only use vector instead of  Vec<BinaryHeap>

--- blind_75_leetcode/src/solution/s0300_longest_increasing_subsequence.rs
+++ blind_75_leetcode/src/solution/s0300_longest_increasing_subsequence.rs
@@ -3,35 +3,25 @@ pub struct Solution {}
 
 impl Solution {
     pub fn length_of_lis(nums: Vec<i32>) -> i32 {
-        use std::cmp::Reverse;
-        use std::collections::BinaryHeap;
-
         if nums.is_empty() {
             return 0;
         }
-        let mut init_heap = BinaryHeap::new();
-        init_heap.push(Reverse(nums[0]));
 
-        let mut dp = vec![];
-        dp.push(init_heap);
+        let mut dp = vec![nums[0]];
 
         for i in 1..nums.len() {
             for count in (0..dp.len()).rev() {
-                if let Some(min) = dp[count].peek() {
-                    if min <= &Reverse(nums[i]) {
-                        if count == 0 {
-                            dp[count].push(Reverse(nums[i]))
-                        }
-                    } else {
-                        if count == dp.len() - 1 {
-                            let mut new_heap = BinaryHeap::new();
-                            new_heap.push(Reverse(nums[i]));
-                            dp.push(new_heap);
-                        } else {
-                            dp[count + 1].push(Reverse(nums[i]))
-                        }
-                        break;
+                if dp[count] >= nums[i] {
+                    if count == 0 {
+                        dp[count] = dp[count].min(nums[i])
                     }
+                } else {
+                    if count == dp.len() - 1 {
+                        dp.push(nums[i]);
+                    } else {
+                        dp[count + 1] = dp[count + 1].min(nums[i])
+                    }
+                    break;
                 }
             }
         }
