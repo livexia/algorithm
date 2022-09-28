657cc7e56b0216beaca046dcad6b655737f365ef
livexia <istillalive@live.cn>
2022-09-28 16:07:16 +0800

    with math

--- blind_75_leetcode/src/solution/s0062_unique_paths.rs
+++ blind_75_leetcode/src/solution/s0062_unique_paths.rs
@@ -3,14 +3,11 @@ pub struct Solution {}
 
 impl Solution {
     pub fn unique_paths(m: i32, n: i32) -> i32 {
-        let (m, n) = (m.max(n) as usize, n.min(m) as usize);
-        let mut cur = vec![1; n];
-        for _ in 1..m {
-            for j in 1..n {
-                cur[j] += cur[j - 1]
-            }
-        }
-        cur[n - 1]
+        // (1..=m + n - 2).product::<i32>()
+        //     / ((1..=m - 1).product::<i32>() * (1..=n - 1).product::<i32>())
+        // (n..=m + n - 2).product::<i32>() / (1..=m - 1).product::<i32>()
+        let (m, n) = (m.min(n) as u64, n.max(m) as u64);
+        (1..m).fold(1, |ans, k| ans * (n + k - 1) / k) as i32
     }
 }
 
@@ -24,5 +21,8 @@ mod tests_62 {
         assert_eq!(Solution::unique_paths(7, 3), 28);
         assert_eq!(Solution::unique_paths(3, 2), 3);
         assert_eq!(Solution::unique_paths(3, 3), 6);
+        assert_eq!(Solution::unique_paths(10, 10), 48620);
+        assert_eq!(Solution::unique_paths(23, 12), 193536720);
+        assert_eq!(Solution::unique_paths(51, 9), 1916797311);
     }
 }
