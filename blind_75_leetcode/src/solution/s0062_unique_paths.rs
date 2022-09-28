add56c16e004ce7518328304745c29d092cda0e8
livexia <istillalive@live.cn>
2022-09-28 15:24:32 +0800

    space optimization 2

--- blind_75_leetcode/src/solution/s0062_unique_paths.rs
+++ blind_75_leetcode/src/solution/s0062_unique_paths.rs
@@ -3,17 +3,14 @@ pub struct Solution {}
 
 impl Solution {
     pub fn unique_paths(m: i32, n: i32) -> i32 {
-        let m = m as usize;
-        let n = n as usize;
-        let mut pre = vec![1; n];
+        let (m, n) = (m.max(n) as usize, n.min(m) as usize);
         let mut cur = vec![1; n];
         for _ in 1..m {
             for j in 1..n {
-                cur[j] = pre[j] + cur[j - 1]
+                cur[j] += cur[j - 1]
             }
-            pre.swap_with_slice(&mut cur);
         }
-        pre[n - 1]
+        cur[n - 1]
     }
 }
 
