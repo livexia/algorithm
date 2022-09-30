a190e8e8d62d261c45054d179a0489e0538c3903
livexia <istillalive@live.cn>
2022-09-30 15:42:20 +0800

    add visited but still wrong

--- blind_75_leetcode/src/solution/s0207_course_schedule.rs
+++ blind_75_leetcode/src/solution/s0207_course_schedule.rs
@@ -10,11 +10,13 @@ impl Solution {
         }
         println!("{:?}", graph);
         for course in 0..num_courses as usize {
-            let mut stack = graph[course].clone();
+            let mut visited = vec![false; num_courses];
+            let mut stack = vec![course];
             while let Some(next_course) = stack.pop() {
-                if next_course == course {
+                if visited[next_course] {
                     return false;
                 }
+                visited[next_course] = true;
                 stack.extend_from_slice(&graph[next_course])
             }
         }
@@ -40,6 +42,7 @@ mod tests_207 {
         assert_eq!(Solution::can_finish(2, vec![vec![1, 0], vec![0, 1]]), false);
         assert_eq!(can_finish(3, vec![[0, 2], [1, 2], [2, 0]]), false);
         assert_eq!(can_finish(4, vec![[1, 0], [2, 1], [3, 2], [1, 3]]), false);
+        assert_eq!(can_finish(5, vec![[1, 4], [2, 4], [3, 1], [3, 2]]), true);
         assert_eq!(
             can_finish(
                 20,
