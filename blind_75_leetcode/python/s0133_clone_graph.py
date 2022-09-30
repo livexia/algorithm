e1cc94c945d75c69a8c0a4b496c80be186bff0dc
livexia <istillalive@live.cn>
2022-09-30 14:14:20 +0800

    add bfs

--- blind_75_leetcode/python/s0133_clone_graph.py
+++ blind_75_leetcode/python/s0133_clone_graph.py
@@ -13,7 +13,8 @@ class Solution:
         if node is None:
             return None
         visited = {}
-        return Solution.bfs(node, visited)
+        # return Solution.dfs(node, visited)
+        return Solution.bfs(node)
 
     def dfs(node: "Node", visited: Dict[int, "Node"]) -> "Node":
         if visited.get(node.val):
@@ -22,6 +23,21 @@ class Solution:
         new_node = Node(val, None)
         visited[val] = new_node
         if node.neighbors is not None:
-            neighbors = [Solution.bfs(child, visited) for child in node.neighbors]
+            neighbors = [Solution.bfs(neighbor, visited) for neighbor in node.neighbors]
             new_node.neighbors = neighbors
         return new_node
+
+    def bfs(node: "Node") -> "Node":
+        from collections import deque
+
+        visited = {}
+        queue = deque([node])
+        visited[node.val] = Node(node.val, None)
+        while queue:
+            n = queue.popleft()
+            for neighbor in n.neighbors:
+                if neighbor.val not in visited:
+                    visited[neighbor.val] = Node(neighbor.val, None)
+                    queue.append(neighbor)
+                visited[n.val].neighbors.append(visited[neighbor.val])
+        return visited[node.val]
