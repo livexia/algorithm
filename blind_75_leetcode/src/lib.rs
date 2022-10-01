6a6767a3b8fc153b35945b68fcd6baec078e5acd
livexia <istillalive@live.cn>
2022-10-01 14:16:14 +0800

    add a shitty macro for leetcode input vec

--- blind_75_leetcode/src/lib.rs
+++ blind_75_leetcode/src/lib.rs
@@ -1,1 +1,14 @@
 mod solution;
+
+#[macro_export]
+macro_rules! leetcode_vec {
+    ($($x: expr), *) => {
+        {
+            let mut temp_vce = Vec::new();
+            $(
+                temp_vce.push($x.to_vec());
+            )*
+            temp_vce
+        }
+    }
+}
