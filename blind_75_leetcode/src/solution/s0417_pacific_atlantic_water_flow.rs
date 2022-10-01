8c3b51ab6c84b4c9495f5df8860b801ee54bff9e
livexia <istillalive@live.cn>
2022-10-01 16:25:50 +0800

    with a slow solution

--- blind_75_leetcode/src/solution/s0417_pacific_atlantic_water_flow.rs
+++ blind_75_leetcode/src/solution/s0417_pacific_atlantic_water_flow.rs
@@ -5,29 +5,17 @@ impl Solution {
     pub fn pacific_atlantic(heights: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
         let m = heights.len();
         let n = heights[0].len();
-        let mut visited = vec![vec![0; n]; m];
-        visited[0] = vec![1; n];
-        (0..m).for_each(|i| visited[i][0] = 1);
+        let mut status = vec![vec![0u8; n]; m];
+        status[0] = vec![1; n];
+        (0..m).for_each(|i| status[i][0] = 1);
+        status[m - 1] = status[m - 1].iter().map(|&v| v | 2).collect();
+        (0..m).for_each(|i| status[i][n - 1] |= 2);
 
-        visited[m - 1] = visited[m - 1].iter().map(|&v| v | 2).collect();
-        (0..m).for_each(|i| visited[i][n - 1] |= 2);
-        for i in 1..m {
-            for j in 1..n {
-                if heights[i][j] >= heights[i - 1][j] {
-                    visited[i][j] |= visited[i - 1][j];
-                }
-                if heights[i][j] >= heights[i][j - 1] {
-                    visited[i][j] |= visited[i][j - 1];
-                }
-            }
-        }
-        for i in (0..m - 1).rev() {
-            for j in (0..n - 1).rev() {
-                if heights[i][j] >= heights[i + 1][j] {
-                    visited[i][j] |= visited[i + 1][j];
-                }
-                if heights[i][j] >= heights[i][j + 1] {
-                    visited[i][j] |= visited[i][j + 1];
+        let mut visited = vec![vec![false; n]; m];
+        for i in 0..m {
+            for j in 0..n {
+                if !visited[i][j] {
+                    Solution::dfs(i, j, &heights, &mut visited, &mut status);
                 }
             }
         }
@@ -35,12 +23,42 @@ impl Solution {
         let mut ans = vec![];
         for i in 0..m {
             for j in 0..n {
-                if visited[i][j] == 3 {
+                if status[i][j] == 3 {
                     ans.push(vec![i as i32, j as i32])
                 }
             }
         }
         ans
+    }
+
+    fn dfs(
+        x: usize,
+        y: usize,
+        heights: &[Vec<i32>],
+        visited: &mut [Vec<bool>],
+        status: &mut [Vec<u8>],
+    ) -> u8 {
+        if visited[x][y] {
+            return status[x][y];
+        }
+        let h = heights[x][y];
+        visited[x][y] = true;
+        if x > 0 && h >= heights[x - 1][y] {
+            status[x][y] |= Solution::dfs(x - 1, y, heights, visited, status)
+        }
+        if y > 0 && h >= heights[x][y - 1] {
+            status[x][y] |= Solution::dfs(x, y - 1, heights, visited, status)
+        }
+        if x < heights.len() - 1 && h >= heights[x + 1][y] {
+            status[x][y] |= Solution::dfs(x + 1, y, heights, visited, status)
+        }
+        if y < heights[0].len() - 1 && h >= heights[x][y + 1] {
+            status[x][y] |= Solution::dfs(x, y + 1, heights, visited, status)
+        }
+        if status[x][y] != 3 {
+            visited[x][y] = false
+        }
+        status[x][y]
     }
 }
 
@@ -66,12 +84,41 @@ mod tests_417 {
             leetcode_vec![[0, 0], [0, 1], [1, 0], [1, 1]]
         );
         assert_eq!(
+            Solution::pacific_atlantic(leetcode_vec![[1, 1], [1, 1], [1, 1]]),
+            leetcode_vec![[0, 0], [0, 1], [1, 0], [1, 1], [2, 0], [2, 1]]
+        );
+        assert_eq!(
             Solution::pacific_atlantic(leetcode_vec![[1]]),
             leetcode_vec![[0, 0]]
         );
         assert_eq!(
             Solution::pacific_atlantic(leetcode_vec![[1, 2, 3], [8, 9, 4], [7, 6, 5]]),
             leetcode_vec![[0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]]
+        );
+        assert_eq!(
+            Solution::pacific_atlantic(leetcode_vec![
+                [3, 3, 3, 3, 3, 3],
+                [3, 0, 3, 3, 0, 3],
+                [3, 3, 3, 3, 3, 3]
+            ]),
+            leetcode_vec![
+                [0, 0],
+                [0, 1],
+                [0, 2],
+                [0, 3],
+                [0, 4],
+                [0, 5],
+                [1, 0],
+                [1, 2],
+                [1, 3],
+                [1, 5],
+                [2, 0],
+                [2, 1],
+                [2, 2],
+                [2, 3],
+                [2, 4],
+                [2, 5]
+            ]
         );
     }
 }
