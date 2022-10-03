86bed88838e05c5cf59ed8c7542eb8a1033fd5a1
livexia <istillalive@live.cn>
2022-10-03 18:02:06 +0800

    with union-find

--- blind_75_leetcode/src/solution/s0200_number_of_islands.rs
+++ blind_75_leetcode/src/solution/s0200_number_of_islands.rs
@@ -8,40 +8,53 @@ impl Solution {
         let n = grid[0].len();
         let mut grid = grid;
         let mut count = 0;
+        let mut union_find = vec![];
         for i in 0..m {
             for j in 0..n {
                 if grid[i][j] == '1' {
+                    union_find.push(i * n + j);
                     count += 1;
-                    // with dfs
-                    // Solution::dfs(i as i32, j as i32, &mut grid, m as i32, n as i32);
-
-                    // with bfs
-                    use std::collections::VecDeque;
-                    let mut queue = VecDeque::new();
-                    queue.push_back((i, j));
-                    while let Some((x, y)) = queue.pop_front() {
-                        if grid[x][y] == '0' {
-                            continue;
-                        }
-                        grid[x][y] = '0';
-
-                        if x > 0 && grid[x - 1][y] == '1' {
-                            queue.push_back((x - 1, y))
-                        }
-                        if x < m - 1 && grid[x + 1][y] == '1' {
-                            queue.push_back((x + 1, y))
-                        }
-                        if y > 0 && grid[x][y - 1] == '1' {
-                            queue.push_back((x, y - 1))
-                        }
-                        if y < n - 1 && grid[x][y + 1] == '1' {
-                            queue.push_back((x, y + 1))
-                        }
+                } else {
+                    union_find.push(m * n);
+                }
+            }
+        }
+        for i in 0..m {
+            for j in 0..n {
+                if grid[i][j] == '1' {
+                    grid[i][j] = '0';
+                    if i > 0 && grid[i - 1][j] == '1' {
+                        Solution::union(&mut union_find, i * n + j, (i - 1) * n + j, &mut count)
+                    }
+                    if i < m - 1 && grid[i + 1][j] == '1' {
+                        Solution::union(&mut union_find, i * n + j, (i + 1) * n + j, &mut count)
+                    }
+                    if j > 0 && grid[i][j - 1] == '1' {
+                        Solution::union(&mut union_find, i * n + j, i * n + j - 1, &mut count)
+                    }
+                    if j < n - 1 && grid[i][j + 1] == '1' {
+                        Solution::union(&mut union_find, i * n + j, i * n + j + 1, &mut count)
                     }
                 }
             }
         }
         count
+    }
+
+    fn find(union_find: &mut [usize], a: usize) -> usize {
+        if union_find[a] != a {
+            union_find[a] = Solution::find(union_find, union_find[a])
+        }
+        union_find[a]
+    }
+
+    fn union(union_find: &mut [usize], a: usize, b: usize, count: &mut i32) {
+        let root_a = Solution::find(union_find, a);
+        let root_b = Solution::find(union_find, b);
+        if root_a != root_b {
+            union_find[root_b] = root_a;
+            *count -= 1;
+        }
     }
 
     fn dfs(i: i32, j: i32, grid: &mut [Vec<char>], m: i32, n: i32) {
