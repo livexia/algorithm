a4af18166b0739ba7cd2b0ed9ee95450bf1a8a20
livexia <istillalive@live.cn>
2022-10-04 14:14:44 +0800

    with bfs

--- blind_75_leetcode/src/solution/s0733_flood_fill.rs
+++ blind_75_leetcode/src/solution/s0733_flood_fill.rs
@@ -5,9 +5,38 @@ impl Solution {
     pub fn flood_fill(image: Vec<Vec<i32>>, sr: i32, sc: i32, color: i32) -> Vec<Vec<i32>> {
         let mut image = image;
         let old_color = image[sr as usize][sc as usize];
-        Solution::dfs(&mut image, sr as usize, sc as usize, old_color, color);
+        // with dfs
+        // Solution::dfs(&mut image, sr as usize, sc as usize, old_color, color);
+
+        //with bfs
+        Solution::bfs(&mut image, sr as usize, sc as usize, old_color, color);
         image
     }
+
+    fn bfs(image: &mut [Vec<i32>], sr: usize, sc: usize, old_color: i32, color: i32) {
+        use std::collections::VecDeque;
+        let mut queue = VecDeque::new();
+        queue.push_back((sr, sc));
+        while let Some((r, c)) = queue.pop_front() {
+            if image[r][c] == color {
+                continue;
+            }
+            image[r][c] = color;
+            if r > 0 && image[r - 1][c] == old_color {
+                queue.push_back((r - 1, c))
+            }
+            if r < image.len() - 1 && image[r + 1][c] == old_color {
+                queue.push_back((r + 1, c))
+            }
+            if c > 0 && image[r][c - 1] == old_color {
+                queue.push_back((r, c - 1))
+            }
+            if c < image[0].len() - 1 && image[r][c + 1] == old_color {
+                queue.push_back((r, c + 1))
+            }
+        }
+    }
+
     fn dfs(image: &mut [Vec<i32>], sr: usize, sc: usize, old_color: i32, color: i32) {
         if image[sr][sc] == color {
             return;
