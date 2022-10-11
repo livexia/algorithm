a924c798886798cbb0789f6faa60c6caaa9cd23b
livexia <istillalive@live.cn>
2022-10-11 19:47:12 +0800

    timeout with dp

--- blind_75_leetcode/src/solution/s0435_non_overlapping_intervals.rs
+++ blind_75_leetcode/src/solution/s0435_non_overlapping_intervals.rs
@@ -3,6 +3,13 @@ pub struct Solution {}
 
 impl Solution {
     pub fn erase_overlap_intervals(intervals: Vec<Vec<i32>>) -> i32 {
+        // with sort left
+        Solution::sort_left(intervals)
+        // with dp
+        // Solution::dp(intervals)
+    }
+
+    fn sort_left(intervals: Vec<Vec<i32>>) -> i32 {
         let mut intervals: Vec<(i32, i32)> = intervals.into_iter().map(|v| (v[0], v[1])).collect();
         intervals.sort();
         let mut count = 0;
@@ -16,6 +23,21 @@ impl Solution {
             }
         }
         count
+    }
+
+    fn dp(intervals: Vec<Vec<i32>>) -> i32 {
+        let mut intervals: Vec<(i32, i32)> = intervals.into_iter().map(|v| (v[0], v[1])).collect();
+        intervals.sort();
+        let n = intervals.len();
+        let mut dp = vec![1; n];
+        for i in 0..n {
+            for j in 0..i {
+                if intervals[i].0 >= intervals[j].1 {
+                    dp[i] = dp[i].max(dp[j] + 1);
+                }
+            }
+        }
+        n as i32 - dp.iter().max().unwrap()
     }
 }
 
