3b5e99c1055368f3d09dfb99f27f54ca81c3895b
livexia <istillalive@live.cn>
2023-03-22 18:56:06 +0800

    19: with unsafe

--- blind_75_leetcode/src/solution/s0019_remove_nth_node_from_end_of_list.rs
+++ blind_75_leetcode/src/solution/s0019_remove_nth_node_from_end_of_list.rs
@@ -62,6 +62,26 @@ impl Solution {
 
         head
     }
+
+    pub fn remove_nth_from_end_unsafe(
+        mut head: Option<Box<ListNode>>,
+        n: i32,
+    ) -> Option<Box<ListNode>> {
+        let mut count = 0;
+        unsafe {
+            let mut fast = &head as *const Option<Box<ListNode>>;
+            let mut slow = &mut head as *mut Option<Box<ListNode>>;
+            while (*fast).is_some() {
+                fast = &(*fast).as_ref().unwrap().next as *const Option<Box<ListNode>>;
+                if count >= n {
+                    slow = &mut (*slow).as_mut().unwrap().next as *mut Option<Box<ListNode>>;
+                }
+                count += 1;
+            }
+            *slow = (*slow).as_mut().unwrap().next.take();
+        }
+        head
+    }
 }
 
 #[cfg(test)]
@@ -85,6 +105,33 @@ mod tests_19 {
         let list = ListNode::from_vec(vec![1, 2]);
         assert_eq!(
             Solution::remove_nth_from_end(list, 2).unwrap().to_vec(),
+            vec![2]
+        );
+    }
+
+    #[test]
+    fn it_works_unsafe() {
+        let list = ListNode::from_vec(vec![1, 2, 3, 4, 5]);
+        assert_eq!(
+            Solution::remove_nth_from_end_unsafe(list, 2)
+                .unwrap()
+                .to_vec(),
+            vec![1, 2, 3, 5]
+        );
+        let list = ListNode::from_vec(vec![1]);
+        assert_eq!(Solution::remove_nth_from_end_unsafe(list, 1), None);
+        let list = ListNode::from_vec(vec![1, 2]);
+        assert_eq!(
+            Solution::remove_nth_from_end_unsafe(list, 1)
+                .unwrap()
+                .to_vec(),
+            vec![1]
+        );
+        let list = ListNode::from_vec(vec![1, 2]);
+        assert_eq!(
+            Solution::remove_nth_from_end_unsafe(list, 2)
+                .unwrap()
+                .to_vec(),
             vec![2]
         );
     }
