d897bb9469c973e8a2e8706081b803e407804240
livexia <istillalive@live.cn>
2023-03-26 20:34:00 +0800

    48: finish

--- blind_75_leetcode/src/solution/s0048_rotate_image.rs
+++ blind_75_leetcode/src/solution/s0048_rotate_image.rs
@@ -4,14 +4,16 @@ pub struct Solution {}
 // Rotate algorithm: https://stackoverflow.com/a/8664879
 // Transpose algorithm: https://en.wikipedia.org/wiki/In-place_matrix_transposition#Square_matrices
 impl Solution {
-    pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
+    pub fn rotate(matrix: &mut [Vec<i32>]) {
         Solution::transpose(matrix);
         Solution::reverse_row(matrix);
     }
 
-    fn transpose(matrix: &mut Vec<Vec<i32>>) {
+    fn transpose(matrix: &mut [Vec<i32>]) {
         for x in 0..matrix.len() {
             for y in x + 1..matrix.len() {
+                // unstable feature
+                // (matrix[x][y], matrix[y][x]) = (matrix[y][x], matrix[x][y]);
                 let temp = matrix[x][y];
                 matrix[x][y] = matrix[y][x];
                 matrix[y][x] = temp;
@@ -19,7 +21,7 @@ impl Solution {
         }
     }
 
-    fn reverse_row(matrix: &mut Vec<Vec<i32>>) {
+    fn reverse_row(matrix: &mut [Vec<i32>]) {
         for row in matrix.iter_mut() {
             row.reverse()
         }
