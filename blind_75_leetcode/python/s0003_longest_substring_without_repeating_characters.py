0801558f51d1976d637d94208db8412cc23a5048
livexia <istillalive@live.cn>
2023-03-30 20:53:04 +0800

    3: python solution with two points

--- blind_75_leetcode/python/s0003_longest_substring_without_repeating_characters.py
+++ blind_75_leetcode/python/s0003_longest_substring_without_repeating_characters.py
@@ -1,20 +1,18 @@
 import unittest
-from typing import Tuple
 
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        def dp(i: int) -> Tuple[int, int, int]:
-            if i == len(s):
-                return (0, 0, 0)
-            hi = 1 << (ord(s[i]) - ord("a"))
-            (m, l, h) = dp(i + 1)
-            if hi & h != 0:
-                return (max(m, 1), 1, hi)
-            else:
-                return (max(m, l + 1), l + 1, h | hi)
-
-        return dp(0)[0]
+        left = right = result = 0
+        while right < len(s):
+            if s[right] in s[left:right]:
+                result = max(result, right - left)
+                while s[left] != s[right]:
+                    left += 1
+                left += 1
+            right += 1
+        result = max(result, right - left)
+        return result
 
 
 class Test0003(unittest.TestCase):
@@ -33,3 +31,6 @@ class Test0003(unittest.TestCase):
 
     def test04(self):
         self.assertEqual(self.helper(" "), 1)
+
+    def test05(self):
+        self.assertEqual(self.helper("abcb"), 3)
