12954aca3d80221fdda362ccdb1513cfe3fb2582
livexia <istillalive@live.cn>
2023-04-01 18:55:04 +0800

    424: rust solution with offical solution

--- blind_75_leetcode/src/solution/s0424_longest_repeating_character_replacement.rs
+++ blind_75_leetcode/src/solution/s0424_longest_repeating_character_replacement.rs
@@ -3,7 +3,22 @@ pub struct Solution {}
 
 impl Solution {
     pub fn character_replacement(s: String, k: i32) -> i32 {
-        todo!()
+        let mut left = 0;
+        let mut result = 0;
+
+        let mut freq = [0; 26];
+        let mut max_count = 0;
+        let s: Vec<_> = s.bytes().map(|b| (b - b'A') as usize).collect();
+        for (right, &b) in s.iter().enumerate() {
+            freq[b] += 1;
+            max_count = max_count.max(freq[b]);
+            if right - left + 1 > max_count as usize + k as usize {
+                freq[s[left]] -= 1;
+                left += 1;
+            }
+            result = result.max(right - left);
+        }
+        result.max(s.len() - left) as i32
     }
 }
 
