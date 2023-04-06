dd5393cbb97ec5a1d33113dcf47d774b968ed35d
livexia <istillalive@live.cn>
2023-04-06 11:30:01 +0800

    125: finish python solution

--- blind_75_leetcode/python/s0125_valid_palindrome.py
+++ blind_75_leetcode/python/s0125_valid_palindrome.py
@@ -2,10 +2,22 @@ import unittest
 
 
 class Solution:
-    # This is a template
-    pass
+    def isPalindrome(self, s: str) -> bool:
+        s = "".join(c.upper() for c in s if c.isalnum())
+        for i in range(len(s) // 2):
+            # negative indexing
+            # ~0 == -1
+            if s[i] != s[~i]:
+                return False
+        return True
+        # return s == s[::-1]
 
 
 class TestS125(unittest.TestCase):
     def test_works(self):
-        self.assertEqual(0, 0)
+        s = Solution()
+        self.assertEqual(s.isPalindrome("A man, a plan, a canal: Panama"), True)
+        self.assertEqual(s.isPalindrome("race a car"), False)
+        self.assertEqual(s.isPalindrome(""), True)
+        self.assertEqual(s.isPalindrome("a."), True)
+        self.assertEqual(s.isPalindrome(".?"), True)
