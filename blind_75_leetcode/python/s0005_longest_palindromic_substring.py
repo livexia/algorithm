a9fc3af723d58d48c485dae8b6ffde4818905f16
livexia <istillalive@live.cn>
2023-04-07 11:16:44 +0800

    5: python with dp

--- blind_75_leetcode/python/s0005_longest_palindromic_substring.py
+++ blind_75_leetcode/python/s0005_longest_palindromic_substring.py
@@ -2,10 +2,23 @@ import unittest
 
 
 class Solution:
-    # This is a template
-    pass
+    def longestPalindrome(self, s: str) -> str:
+        def dp(i, j) -> bool:
+            return i == j or s[i] == s[j] and (i + 1 == j or dp(i + 1, j - 1))
+
+        result = (0, 0)
+        for i in range(len(s)):
+            if len(s) - 1 - i < result[1] - result[0]:
+                break
+            for j in reversed(range(result[1] - result[0] + i, len(s))):
+                if dp(i, j):
+                    result = (i, j)
+                    break
+        return "".join(s[result[0] : result[1] + 1])
 
 
 class TestS5(unittest.TestCase):
     def test_works(self):
-        self.assertEqual(0, 0)
+        s = Solution()
+        self.assertEqual(s.longestPalindrome("babad") in ["bab", "aba"], True)
+        self.assertEqual(s.longestPalindrome("cbbd"), "bb")
