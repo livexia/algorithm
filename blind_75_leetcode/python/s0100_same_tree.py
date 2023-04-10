b8305a010e756ab0e3c5ae90bf1eab1c99ac70ff
livexia <istillalive@live.cn>
2023-04-10 10:24:06 +0800

    100: python with monkey patch __eq__

--- blind_75_leetcode/python/s0100_same_tree.py
+++ blind_75_leetcode/python/s0100_same_tree.py
@@ -4,18 +4,29 @@ from typing import Optional
 from .s0104_maximum_depth_of_binary_tree import TreeNode, create_tree_from_list
 
 
+def treenode_eq(p, q) -> bool:
+    return bool(
+        (not p and not q)
+        or (p and q and p.val == q.val and p.left == q.left and p.right == q.right)
+    )
+
+
+TreeNode.__eq__ = treenode_eq
+
+
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if (not p and not q) or (
-            p
-            and q
-            and p.val == q.val
-            and self.isSameTree(p.left, q.left)
-            and self.isSameTree(p.right, q.right)
-        ):
-            return True
-        else:
-            return False
+        return p == q
+        # return bool(
+        #     (not p and not q)
+        #     or (
+        #         p
+        #         and q
+        #         and p.val == q.val
+        #         and self.isSameTree(p.left, q.left)
+        #         and self.isSameTree(p.right, q.right)
+        #     )
+        # )
 
 
 class TestS100(unittest.TestCase):
