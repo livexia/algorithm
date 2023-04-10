1aea7b9f686855ad0667d1b194b2f2b7d98d9afc
livexia <istillalive@live.cn>
2023-04-10 10:08:06 +0800

    100: python with recursion

--- blind_75_leetcode/python/s0100_same_tree.py
+++ blind_75_leetcode/python/s0100_same_tree.py
@@ -1,11 +1,28 @@
 import unittest
+from typing import Optional
+
+from .s0104_maximum_depth_of_binary_tree import TreeNode, create_tree_from_list
 
 
 class Solution:
-    # This is a template
-    pass
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if (not p and not q) or (
+            p
+            and q
+            and p.val == q.val
+            and self.isSameTree(p.left, q.left)
+            and self.isSameTree(p.right, q.right)
+        ):
+            return True
+        else:
+            return False
 
 
 class TestS100(unittest.TestCase):
     def test_works(self):
-        self.assertEqual(0, 0)
+        self.assertEqual(
+            Solution().isSameTree(
+                create_tree_from_list([1, 2, 3]), create_tree_from_list([1, 2, 3])
+            ),
+            True,
+        )
