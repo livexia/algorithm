4e809a75104c38e9c22cd44db9d173c39d0c642f
livexia <istillalive@live.cn>
2023-04-14 15:47:58 +0800

    297: python with pre-oreder dfs

--- blind_75_leetcode/python/s0297_serialize_and_deserialize_binary_tree.py
+++ blind_75_leetcode/python/s0297_serialize_and_deserialize_binary_tree.py
@@ -1,4 +1,7 @@
+from re import split
 import unittest
+
+from .s0104_maximum_depth_of_binary_tree import TreeNode, create_tree_from_list
 
 
 class Solution:
@@ -6,6 +9,59 @@ class Solution:
     pass
 
 
+class Codec:
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+
+        def preorder(node):
+            if node:
+                return (
+                    f"{node.val}"
+                    + ","
+                    + preorder(node.left)
+                    + ","
+                    + preorder(node.right)
+                )
+            else:
+                return "None"
+
+        return preorder(root)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+
+        def preorder():
+            val = data.pop()
+            if val == "None":
+                return None
+            node = TreeNode(int(val))
+            node.left = preorder()
+            node.right = preorder()
+
+            return node
+
+        data = data.split(",")
+        data.reverse()
+        return preorder()
+
+
 class TestS297(unittest.TestCase):
     def test_works(self):
-        self.assertEqual(0, 0)
+        codec = Codec()
+        self.assertEqual(
+            codec.serialize(create_tree_from_list([1, 2, 3, None, None, 4, 5])),
+            "1,2,None,None,3,4,None,None,5,None,None",
+        )
+
+        self.assertEqual(
+            codec.deserialize("1,2,None,None,3,4,None,None,5,None,None"),
+            create_tree_from_list([1, 2, 3, None, None, 4, 5]),
+        )
