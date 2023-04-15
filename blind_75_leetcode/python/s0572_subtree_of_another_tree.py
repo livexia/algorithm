fd971ee5d13bc37699fd1cac4331e9b6693eb69a
livexia <istillalive@live.cn>
2023-04-15 18:05:30 +0800

    572: python with dfs

--- blind_75_leetcode/python/s0572_subtree_of_another_tree.py
+++ blind_75_leetcode/python/s0572_subtree_of_another_tree.py
@@ -1,11 +1,48 @@
 import unittest
+from typing import Optional
+
+
+from .s0104_maximum_depth_of_binary_tree import TreeNode, create_tree_from_list
 
 
 class Solution:
-    # This is a template
-    pass
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def is_identical(t1: Optional[TreeNode], t2: Optional[TreeNode]) -> bool:
+            if not t1 and not t2:
+                return True
+            elif t1 and t2:
+                return (
+                    t1.val == t2.val
+                    and is_identical(t1.left, t2.left)
+                    and is_identical(t1.right, t2.right)
+                )
+            else:
+                return False
+
+        if not subRoot:
+            return True
+        elif not root:
+            return False
+        else:
+            return (
+                is_identical(root, subRoot)
+                or self.isSubtree(root.left, subRoot)
+                or self.isSubtree(root.right, subRoot)
+            )
 
 
 class TestS572(unittest.TestCase):
     def test_works(self):
-        self.assertEqual(0, 0)
+        self.assertEqual(
+            Solution().isSubtree(
+                create_tree_from_list([3, 4, 5, 1, 2]), create_tree_from_list([4, 1, 2])
+            ),
+            True,
+        )
+        self.assertEqual(
+            Solution().isSubtree(
+                create_tree_from_list([3, 4, 5, 1, 2, None, None, None, None, 0]),
+                create_tree_from_list([4, 1, 2]),
+            ),
+            True,
+        )
