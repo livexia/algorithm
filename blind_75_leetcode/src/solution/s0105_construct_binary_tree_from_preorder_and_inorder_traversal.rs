92bf875cd3070bbc06bb15ca847d99cf5684b897
livexia <istillalive@live.cn>
2023-04-17 19:44:08 +0800

    105: second try

--- blind_75_leetcode/src/solution/s0105_construct_binary_tree_from_preorder_and_inorder_traversal.rs
+++ blind_75_leetcode/src/solution/s0105_construct_binary_tree_from_preorder_and_inorder_traversal.rs
@@ -4,32 +4,48 @@ pub struct Solution {}
 use super::s0104_maximum_depth_of_binary_tree::TreeNode;
 
 use std::cell::RefCell;
+use std::collections::HashSet;
 use std::rc::Rc;
 impl Solution {
     pub fn build_tree(preorder: Vec<i32>, inorder: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
-        let mut pre_index = 0;
-        let mut in_index = 0;
-        let mut in_visited = std::collections::HashSet::new();
-        while in_visited.len() != preorder.len() {
-            let mut root = preorder[pre_index];
-            in_visited.insert(root);
-            println!("root: {root}");
-            pre_index += 1;
-            let left = preorder[pre_index];
-            in_visited.insert(left);
-            println!("left: {left}");
-            let in_index = inorder.iter().position(|&i| i == left).unwrap();
-            if in_index == 0 || in_visited.contains(&inorder[in_index - 1]) {
-                pre_index += 1;
-                let right = preorder[pre_index];
-                in_visited.insert(right);
-                println!("right: {right}")
-            } else {
-                println!("++++");
-            }
-        }
+        Solution::build_node(0, &preorder, &inorder, &mut HashSet::new()).1
+    }
 
-        todo!()
+    fn build_node(
+        mut pre_index: usize,
+        preorder: &[i32],
+        inorder: &[i32],
+        visited: &mut HashSet<i32>,
+    ) -> (usize, Option<Rc<RefCell<TreeNode>>>) {
+        if pre_index > preorder.len() - 1 || visited.len() == preorder.len() {
+            return (pre_index, None);
+        }
+        let root = preorder[pre_index];
+        let mut node = TreeNode::new(root);
+        visited.insert(root);
+        let in_index = inorder.iter().position(|&i| i == root).unwrap();
+        if in_index == 0 || visited.contains(&inorder[in_index - 1]) {
+            pre_index += 1;
+            (pre_index, node.right) = if pre_index < preorder.len() {
+                Solution::build_node(pre_index, preorder, inorder, visited)
+            } else {
+                (pre_index, None)
+            };
+        } else {
+            (pre_index, node.left) = if pre_index + 1 < preorder.len() {
+                Solution::build_node(pre_index + 1, preorder, inorder, visited)
+            } else {
+                (pre_index, None)
+            };
+            (pre_index, node.right) = if pre_index < preorder.len() {
+                Solution::build_node(pre_index, preorder, inorder, visited)
+            } else {
+                (pre_index, None)
+            };
+
+            println!("{root} left: {:?}", node.left);
+        }
+        return (pre_index, Some(Rc::new(RefCell::new(node))));
     }
 }
 #[cfg(test)]
@@ -38,18 +54,18 @@ mod tests_105 {
 
     #[test]
     fn it_works() {
-        // assert_eq!(
-        //     Solution::build_tree(vec![3, 9, 20, 15, 7], vec![9, 3, 15, 20, 7]),
-        //     TreeNode::from_vec(vec![
-        //         Some(3),
-        //         Some(9),
-        //         Some(20),
-        //         None,
-        //         None,
-        //         Some(15),
-        //         Some(7)
-        //     ])
-        // );
+        assert_eq!(
+            Solution::build_tree(vec![3, 9, 20, 15, 7], vec![9, 3, 15, 20, 7]),
+            TreeNode::from_vec(vec![
+                Some(3),
+                Some(9),
+                Some(20),
+                None,
+                None,
+                Some(15),
+                Some(7)
+            ])
+        );
         assert_eq!(
             Solution::build_tree(vec![3, 9, 15, 7, 20], vec![15, 9, 7, 3, 20]),
             TreeNode::from_vec(vec![Some(3), Some(9), Some(20), Some(15), Some(7)])
