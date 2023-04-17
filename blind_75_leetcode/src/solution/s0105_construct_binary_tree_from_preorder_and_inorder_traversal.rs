4cadd51e151a04dc6bf5093dcbeafde0ebe7edf3
livexia <istillalive@live.cn>
2023-04-17 20:18:21 +0800

    105: rust with super slow solution

--- blind_75_leetcode/src/solution/s0105_construct_binary_tree_from_preorder_and_inorder_traversal.rs
+++ blind_75_leetcode/src/solution/s0105_construct_binary_tree_from_preorder_and_inorder_traversal.rs
@@ -4,50 +4,48 @@ pub struct Solution {}
 use super::s0104_maximum_depth_of_binary_tree::TreeNode;
 
 use std::cell::RefCell;
-use std::collections::HashSet;
+use std::collections::HashMap;
 use std::rc::Rc;
 impl Solution {
     pub fn build_tree(preorder: Vec<i32>, inorder: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
-        Solution::build_node(0, &preorder, &inorder, &mut HashSet::new()).1
+        Solution::build_node(&preorder, &inorder)
     }
 
     fn build_node(
-        mut pre_index: usize,
         preorder: &[i32],
         inorder: &[i32],
-        visited: &mut HashSet<i32>,
-    ) -> (usize, Option<Rc<RefCell<TreeNode>>>) {
-        if pre_index > preorder.len() - 1 || visited.len() == preorder.len() {
-            return (pre_index, None);
-        }
-        let root = preorder[pre_index];
-        let mut node = TreeNode::new(root);
-        visited.insert(root);
-        let in_index = inorder.iter().position(|&i| i == root).unwrap();
-        if in_index == 0 || visited.contains(&inorder[in_index - 1]) {
-            pre_index += 1;
-            (pre_index, node.right) = if pre_index < preorder.len() {
-                Solution::build_node(pre_index, preorder, inorder, visited)
-            } else {
-                (pre_index, None)
-            };
+        // preorder_index: &HashMap<i32, usize>,
+        // inorder_index: &HashMap<i32, usize>,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        if preorder.len() == 1 {
+            Some(Rc::new(RefCell::new(TreeNode::new(preorder[0]))))
+        } else if preorder.is_empty() {
+            None
         } else {
-            (pre_index, node.left) = if pre_index + 1 < preorder.len() {
-                Solution::build_node(pre_index + 1, preorder, inorder, visited)
+            let preorder_index: HashMap<i32, usize> =
+                preorder.iter().enumerate().map(|(i, &v)| (v, i)).collect();
+            let inorder_index: HashMap<i32, usize> =
+                inorder.iter().enumerate().map(|(i, &v)| (v, i)).collect();
+            let root = preorder[0];
+            let mut node = TreeNode::new(root);
+            let &in_index = inorder_index.get(&root).unwrap();
+            let left = if let Some(&left) = inorder[..in_index]
+                .iter()
+                .map(|i| preorder_index.get(i).unwrap())
+                .max()
+            {
+                node.left = Solution::build_node(&preorder[1..=left], &inorder[..in_index]);
+                left
             } else {
-                (pre_index, None)
+                node.left = None;
+                0
             };
-            (pre_index, node.right) = if pre_index < preorder.len() {
-                Solution::build_node(pre_index, preorder, inorder, visited)
-            } else {
-                (pre_index, None)
-            };
-
-            println!("{root} left: {:?}", node.left);
+            node.right = Solution::build_node(&preorder[left + 1..], &inorder[in_index + 1..]);
+            Some(Rc::new(RefCell::new(node)))
         }
-        return (pre_index, Some(Rc::new(RefCell::new(node))));
     }
 }
+
 #[cfg(test)]
 mod tests_105 {
     use super::*;
@@ -73,6 +71,10 @@ mod tests_105 {
         assert_eq!(
             Solution::build_tree(vec![-1], vec![-1]),
             TreeNode::from_vec(vec![Some(-1)])
+        );
+        assert_eq!(
+            Solution::build_tree(vec![1, 2], vec![1, 2]),
+            TreeNode::from_vec(vec![Some(1), None, Some(2)])
         );
     }
 }
