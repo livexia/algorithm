b2ba553d0f22bada2a0dc45028ff142a158196e9
livexia <istillalive@live.cn>
2023-04-18 19:28:15 +0800

    105: python with dfs and dict for inorder index

--- blind_75_leetcode/python/s0105_construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ blind_75_leetcode/python/s0105_construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -7,6 +7,7 @@ from .s0104_maximum_depth_of_binary_tree import TreeNode, create_tree_from_list
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         self.pre_index = 0
+        inorder_index = {v: i for (i, v) in enumerate(inorder)}
 
         def build_node(in_start: int, in_end: int) -> Optional[TreeNode]:
             if in_start == in_end:
@@ -14,7 +15,7 @@ class Solution:
             else:
                 node = TreeNode(preorder[self.pre_index])
                 self.pre_index += 1
-                in_index = inorder.index(node.val)
+                in_index = inorder_index[node.val]
                 node.left = build_node(in_start, in_index)
                 node.right = build_node(in_index + 1, in_end)
                 return node
