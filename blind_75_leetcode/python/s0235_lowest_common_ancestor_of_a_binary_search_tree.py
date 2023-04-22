45c25a42c4ce1d56fb4c904b2831100d744b5b27
livexia <istillalive@live.cn>
2023-04-22 21:42:47 +0800

    235: python with recursion

--- blind_75_leetcode/python/s0235_lowest_common_ancestor_of_a_binary_search_tree.py
+++ blind_75_leetcode/python/s0235_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -1,11 +1,36 @@
 import unittest
 
+from .s0104_maximum_depth_of_binary_tree import (
+    TreeNode,
+    create_tree_from_list,
+    search_val,
+)
+
 
 class Solution:
-    # This is a template
-    pass
+    def lowestCommonAncestor(
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+    ) -> "TreeNode":
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return root
 
 
 class TestS235(unittest.TestCase):
     def test_works(self):
-        self.assertEqual(0, 0)
+        root = create_tree_from_list([6, 2, 8, 0, 4, 7, 9, None, None, 3, 5])
+        self.assertEqual(
+            Solution().lowestCommonAncestor(
+                root, search_val(root, 2), search_val(root, 8)
+            ),
+            search_val(root, 6),
+        )
+        self.assertEqual(
+            Solution().lowestCommonAncestor(
+                root, search_val(root, 2), search_val(root, 4)
+            ),
+            search_val(root, 2),
+        )
