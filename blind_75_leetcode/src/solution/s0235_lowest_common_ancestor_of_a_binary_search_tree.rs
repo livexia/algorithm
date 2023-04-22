e47556911a2b3e0a9bf5615249bfac507e451ecb
livexia <istillalive@live.cn>
2023-04-22 21:26:39 +0800

    234: rust with dfs

--- blind_75_leetcode/src/solution/s0235_lowest_common_ancestor_of_a_binary_search_tree.rs
+++ blind_75_leetcode/src/solution/s0235_lowest_common_ancestor_of_a_binary_search_tree.rs
@@ -1,8 +1,9 @@
 #![allow(dead_code)]
 pub struct Solution {}
 
-use super::s0104_maximum_depth_of_binary_tree::{search_val, TreeNode};
+use super::s0104_maximum_depth_of_binary_tree::TreeNode;
 use std::cell::RefCell;
+use std::cmp::Ordering;
 use std::rc::Rc;
 impl Solution {
     pub fn lowest_common_ancestor(
@@ -10,12 +11,47 @@ impl Solution {
         p: Option<Rc<RefCell<TreeNode>>>,
         q: Option<Rc<RefCell<TreeNode>>>,
     ) -> Option<Rc<RefCell<TreeNode>>> {
-        todo!()
+        fn dfs(
+            root: &Option<Rc<RefCell<TreeNode>>>,
+            p: &Option<Rc<RefCell<TreeNode>>>,
+            q: &Option<Rc<RefCell<TreeNode>>>,
+        ) -> Option<Rc<RefCell<TreeNode>>> {
+            if let Some(node) = root {
+                if let Ordering::Less = cmp_with_option(root, p) {
+                    dfs(&node.borrow().right, p, q)
+                } else if let Ordering::Greater = cmp_with_option(root, q) {
+                    dfs(&node.borrow().left, p, q)
+                } else {
+                    root.clone()
+                }
+            } else {
+                None
+            }
+        }
+        let (p, q) = if let Ordering::Greater = cmp_with_option(&p, &q) {
+            (q, p)
+        } else {
+            (p, q)
+        };
+        dfs(&root, &p, &q)
+    }
+}
+
+fn cmp_with_option(
+    p: &Option<Rc<RefCell<TreeNode>>>,
+    q: &Option<Rc<RefCell<TreeNode>>>,
+) -> Ordering {
+    match (p, q) {
+        (None, None) => Ordering::Equal,
+        (Some(_), None) => Ordering::Greater,
+        (None, Some(_)) => Ordering::Less,
+        (Some(p), Some(q)) => p.borrow().val.cmp(&q.borrow().val),
     }
 }
 
 #[cfg(test)]
 mod tests_235 {
+    use super::super::s0104_maximum_depth_of_binary_tree::search_val;
     use super::*;
 
     #[test]
