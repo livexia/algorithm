f24d9431f71cef781ef2693aa8ba34ab8c4b8d43
livexia <istillalive@live.cn>
2023-04-23 12:51:48 +0800

    208: python

--- blind_75_leetcode/python/s0208_implement_trie_prefix_tree.py
+++ blind_75_leetcode/python/s0208_implement_trie_prefix_tree.py
@@ -1,11 +1,41 @@
 import unittest
+from typing import Optional
 
 
-class Solution:
-    # This is a template
-    pass
+class Trie:  # type: ignore
+    def __init__(self):
+        self.is_word = False
+        self.children = [None] * 26
+
+    def insert(self, word: str) -> None:
+        node = self
+        for index in (ord(c) - ord("a") for c in word):
+            if node.children[index] is None:
+                node.children[index] = Trie()
+            node = node.children[index]
+        node.is_word = True
+
+    def _search(self, word: str) -> Optional[bool]:
+        node = self
+        for index in (ord(c) - ord("a") for c in word):
+            if node.children[index] is None:
+                return None
+            node = node.children[index]
+        return node.is_word
+
+    def search(self, word: str) -> bool:
+        return bool(self._search(word))
+
+    def startsWith(self, prefix: str) -> bool:
+        return self._search(prefix) is not None
 
 
 class TestS208(unittest.TestCase):
     def test_works(self):
-        self.assertEqual(0, 0)
+        trie = Trie()
+        trie.insert("apple")
+        self.assertEqual(trie.search("apple"), True)
+        self.assertEqual(trie.search("app"), False)
+        self.assertEqual(trie.startsWith("app"), True)
+        trie.insert("app")
+        self.assertEqual(trie.search("app"), True)
