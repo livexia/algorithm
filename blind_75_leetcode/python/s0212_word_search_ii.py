1dda88d369bff7c63b7a7d4580e9b833275db2d3
livexia <istillalive@live.cn>
2023-04-26 11:11:38 +0800

    212: python timeout

--- blind_75_leetcode/python/s0212_word_search_ii.py
+++ blind_75_leetcode/python/s0212_word_search_ii.py
@@ -1,11 +1,76 @@
+from typing import Optional, List
 import unittest
 
 
 class Solution:
-    # This is a template
-    pass
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        trie = Trie()
+        for w in words:
+            trie.add(w)
+        return trie.search_board(board)
+
+
+class Trie:
+    word: Optional[str]
+    chidren: List[Optional["Trie"]]
+
+    def __init__(self) -> None:
+        self.word = None
+        self.chidren = [None] * 26
+        pass
+
+    def add(self, word: str):
+        node: Trie = self
+        for c in (ord(c) - ord("a") for c in word):
+            if node.chidren[c] is None:
+                node.chidren[c] = Trie()
+            node = node.chidren[c]  # type: ignore
+        node.word = word
+
+    def search_board(self, board: List[List[str]]) -> List[str]:
+        m, n = len(board), len(board[0])
+        searched = []
+        for i in range(m):
+            for j in range(n):
+                self._search_board(board, i, j, searched)
+        return searched
+
+    def _search_board(
+        self, board: List[List[str]], i: int, j: int, searched: List[str]
+    ):
+        m, n = len(board), len(board[0])
+        c = board[i][j]
+        board[i][j] = "#"
+        index = ord(c) - ord("a")
+        node = self.chidren[index]
+        if node is not None:
+            if node.word is not None:
+                searched.append(node.word)
+                node.word = None
+            for (x, y) in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+                if 0 <= x < m and 0 <= y < n and board[x][y] != "#":
+                    node._search_board(board, x, y, searched)
+
+        board[i][j] = c
 
 
 class TestS212(unittest.TestCase):
     def test_works(self):
-        self.assertEqual(0, 0)
+        self.assertEqual(
+            set(
+                Solution().findWords(
+                    board=[
+                        ["o", "a", "a", "n"],
+                        ["e", "t", "a", "e"],
+                        ["i", "h", "k", "r"],
+                        ["i", "f", "l", "v"],
+                    ],
+                    words=["oath", "pea", "eat", "rain"],
+                )
+            ),
+            set(["eat", "oath"]),
+        )
+        self.assertEqual(
+            Solution().findWords(board=[["a", "b"], ["c", "d"]], words=["abcb"]),
+            [],
+        )
