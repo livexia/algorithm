966d304352087eddedf8c6c68f5279e7cf56d53c
livexia <istillalive@live.cn>
2023-04-27 11:52:55 +0800

    347: python counter with heapq

--- blind_75_leetcode/python/s0347_top_k_frequent_elements.py
+++ blind_75_leetcode/python/s0347_top_k_frequent_elements.py
@@ -1,11 +1,20 @@
 import unittest
+from typing import List
+
+from heapq import nlargest
+from collections import Counter
 
 
 class Solution:
-    # This is a template
-    pass
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = Counter(nums)
+        heap = [(v, k) for k, v in counter.items()]
+        return [k for _, k in nlargest(k, heap)]
 
 
 class TestS347(unittest.TestCase):
     def test_works(self):
-        self.assertEqual(0, 0)
+        self.assertEqual(
+            set(Solution().topKFrequent([1, 1, 1, 2, 2, 3], 2)), set([1, 2])
+        )
+        self.assertEqual(Solution().topKFrequent([1], 1), [1])
